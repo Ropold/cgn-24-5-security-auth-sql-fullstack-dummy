5cb3e71509e3416b04644b05465113ef4586b69d
Flooooooooooorian <florian.weber@neuefische.de>
2024-11-12 10:25:48 +0100

    Add sonar-cloud

--- backend/src/main/java/um/backend/BackendApplication.java
+++ backend/src/main/java/um/backend/BackendApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
     public static void main(String[] args) {
+        String ABC = "!";
         SpringApplication.run(BackendApplication.class, args);
     }
 
