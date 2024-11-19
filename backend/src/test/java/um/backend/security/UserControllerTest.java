9eafd5101f6690e5fee6fd71b926249d06805acb
Flooooooooooorian <florian.weber@neuefische.de>
2024-11-19 10:59:06 +0100

    Add Auth Tests

---
+++ backend/src/test/java/um/backend/security/UserControllerTest.java
@@ -0,0 +1,36 @@
+package um.backend.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.oidcLogin;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class UserControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+//    @WithMockUser
+    void testMe() throws Exception {
+        //GIVEN
+
+        //WHEN
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/users/me")
+                .with(oidcLogin()
+                        .userInfoToken(token -> token
+                                .claim("login", "test-user")
+                                .claim("avatar_url", "img"))));
+
+        //THEN
+    }
+
+}
